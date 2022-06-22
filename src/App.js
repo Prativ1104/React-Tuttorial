06827a20566dec0d9170809a90d30cd65ee7e4e8
prativ <prativ.stha03@gmail.com>
2022-06-22 22:59:01 +0545

    The Items are updated to food items along with prices

--- src/App.js
+++ src/App.js
@@ -5,30 +5,38 @@ function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
+      title: "A-One Special Burger",
+      amount: 290,
+      date: new Date(2022, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { 
+      id: "e2", 
+      title: "Mixed Pizza", 
+      amount: 490, 
+      date: new Date(2022, 3, 12), 
+    },
     {
       id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
+      title: "Fried C Momo",
+      amount: 230,
+      date: new Date(2022, 2, 28),
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
+      title: "Chicken Sizzler",
+      amount: 510,
+      date: new Date(2022, 5, 12),
     },
   ];
 
   return (
     <div className="App">
-      <h1>Lets get started</h1>
-      <p>This is also visible</p>
-      <Expenses expenses={expenses}/>
+        <h1>Restaurant Menu</h1>
+      <p>These are the items in our menu with their respective price.</p>
+      <Expenses expenses={expenses}>
+    
+      </Expenses>
+      
     </div>  
   );
 }
