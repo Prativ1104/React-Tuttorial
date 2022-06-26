f84f810172618d562bcf579f06d65d78ca3e363d
Prativ <prativ.stha03@gail.com>
2022-06-26 15:14:16 +0545

    filter added

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ function App() {
       id: "e2",
       title: "Mixed Pizza",
       amount: 490,
-      date: new Date(2022, 3, 12),
+      date: new Date(2021, 3, 12),
     },
     {
       id: "e3",
       title: "Fried C Momo",
       amount: 230,
-      date: new Date(2022, 2, 28),
+      date: new Date(2019, 2, 28),
     },
     {
       id: "e4",
       title: "Chicken Sizzler",
       amount: 510,
-      date: new Date(2022, 5, 12),
+      date: new Date(2020, 5, 12),
     },
   ];
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,21 @@
-import './Expenses.css';
-import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from './ExpensesFilter';
+import Card from "../UI/Card";
+import "./Expenses.css";
+import { useState } from "react";
 
+function Expenses(props) {
 
-function Expenses(props)
-{
-    return(
-        <Card className='expenses'>
-            <ExpenseItem
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
         date={props.expenses[0].date}
@@ -17,21 +25,18 @@ function Expenses(props)
         amount={props.expenses[1].amount}
         date={props.expenses[1].date}
       ></ExpenseItem>
-
       <ExpenseItem
         title={props.expenses[2].title}
         amount={props.expenses[2].amount}
         date={props.expenses[2].date}
       ></ExpenseItem>
-
       <ExpenseItem
         title={props.expenses[3].title}
         amount={props.expenses[3].amount}
         date={props.expenses[3].date}
-      />
-      
+      ></ExpenseItem>
     </Card>
-    )
+  );
 }
 
 export default Expenses;
