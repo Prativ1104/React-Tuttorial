f5ce29ec114569aa851b2c75b4eb7c6313ee297d
Prativ <prativ.stha03@gail.com>
2022-07-12 21:47:40 +0545

    Chart Created

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
 import "./ExpenseItem.css";
-import { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
@@ -7,6 +7,7 @@ function ExpenseItem(props) {
 
 
   return (
+    <li>
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
@@ -15,6 +16,7 @@ function ExpenseItem(props) {
       </div>
       
     </Card>
+    </li>
   );
 }
 
